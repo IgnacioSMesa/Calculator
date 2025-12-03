53c8ab977492b865ef9fce2875a907760c08088c
IgnacioSMesa <ignaciosanzmesa@gmail.com>
2025-12-03 09:21:07 +0100

    Codigo arreglado

--- js/calculator.js
+++ js/calculator.js
@@ -3,6 +3,19 @@ let nums = [];
 let currentOperator = null;
 let currentNumber = "";
 let display = document.querySelector(".display");
+let equalButton = document.getElementById('buttonEqual');
+
+equalButton.disabled = true;
+
+// funcion para comprobar que solo se pueda presionar el boton igual cuando haya suficientes datos
+function updateEqualState() {
+    if (nums.length === 1 && currentOperator !== null && currentNumber !== "") {
+        equalButton.disabled = false;
+    } else {
+        equalButton.disabled = true;
+    }
+}
+
 
 for (let i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener('click', function() {
@@ -13,6 +26,7 @@ for (let i = 0; i < numbers.length; i++) {
             currentNumber += selectedNumber;
         }
         display.innerText = currentNumber;
+        updateEqualState();
     });
 }
 
@@ -27,42 +41,53 @@ for (let i = 0; i < sign.length; i++) {
             nums = [];
             currentOperator = null;
             display.innerText = "0";
+            equalButton.disabled = true;
+
         } else if (operator === "±") {
             currentNumber = (parseFloat(currentNumber) * -1).toString();
             display.innerText = currentNumber;
+
         } else if (operator === "%") {
             currentNumber = (parseFloat(currentNumber) / 100).toString();
             display.innerText = currentNumber;
+
         } else {
+
             if (currentNumber !== "") {
                 nums.push(parseFloat(currentNumber));
                 currentNumber = "";
             }
             currentOperator = operator;
             display.innerText = currentOperator;
+            
+            updateEqualState();
         }
         
     });
 }
 
-let equalButton = document.getElementById('buttonEqual');
-
 equalButton.addEventListener('click', function() {
     if (currentNumber !== "") {
         nums.push(parseFloat(currentNumber));
         currentNumber = "";
+        equalButton.disabled = false;
     }
 
     let result;
     if (nums.length >= 2) {
+
         if (currentOperator === '+') {
             result = nums.reduce((acc, num) => acc + num, 0);
+
         } else if (currentOperator === '−') {
             result = nums.reduce((acc, num) => acc - num);
+
         } else if (currentOperator === '×') {
             result = nums.reduce((acc, num) => acc * num, 1);
+
         } else if (currentOperator === '÷') {
             result = nums.reduce((acc, num) => acc / num);
+
         }
     }
 
